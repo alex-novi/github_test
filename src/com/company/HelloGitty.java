1481cdf2ebc2ccf3824c3a712dea46951e4fbb2a
alex-novi <a.dewaal@novi-education.nl>
2021-01-10 12:23:39 +0100

    Overloaded tale() ter the commit message for your changes. Lines starting

--- src/com/company/HelloGitty.java
+++ src/com/company/HelloGitty.java
@@ -12,12 +12,16 @@ public class HelloGitty {
         return "Gittens got glaws.";
     }
 
-    public String tale(String[] strings, int n){
-        if (n == 4) return "";
-        else return strings[n] + tale(strings, n + 1);
+    public String tale(){
+        return tale(HelloGitty.words);
     }
-    
+
     public String tale(String[] strings){
-        return tale(strings, strings.length)
+        return tale(strings, 0);
+    }
+
+    public String tale(String[] strings, int n){
+        if (n == strings.length) return "";
+        else return strings[n] + tale(strings, n + 1);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
 	HelloGitty welcome = new HelloGitty();
         System.out.println("welcome.purr = " + welcome.purr);
         System.out.println(welcome.scratch());
-        System.out.println(welcome.tale(HelloGitty.words, HelloGitty.words.length));
+        System.out.println(welcome.tale(HelloGitty.words));
     }
 }
